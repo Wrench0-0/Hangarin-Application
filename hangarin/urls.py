df3f4a0abe15a79b3e03148a9250e116a7bfc2bc
Wrench0-0 <202380356@psu.palawan.edu.ph>
2026-03-08 18:35:53 +0800

    pythonanywhere

--- hangarin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from tasks.views import HomeView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('tasks/', include('tasks.urls')),
-    path('admin/', admin.site.urls),
-]
